c5ff3e4e6d2334aab0890d6b611607fd6ec7440d
I-Unknown-I <62710051+I-Unknown-I@users.noreply.github.com>
2024-12-04 22:05:11 +0300

    0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p1 = new Point(5, 8);
+        Point p2 = new Point(4, 0);
+        Point p3 = new Point(0, 3);
+        double area = Point.triangleArea(p1, p2, p3);
+        p1.distanseTo(p2);
+        System.out.println("Площадь треугольника: " + area);
+
+        Point3D p4 = new Point3D(0, 0, 0);
+        Point3D p5 = new Point3D(10, 0, 0);
+        Point3D p6 = new Point3D(0, 10, 3);
+        Point3D p7 = new Point3D(0, 5, 1);
+        double volume = Point3D.tetrahedronVolume(p4, p5, p6, p7);
+        System.out.println("Объем тетраэдра: " + volume);
+
+        System.out.println(p1.distanseTo(p2)+ " Расстояние между двумя точками"+"\n"+ p7.distanseTo(p6)+" Расстояние между двумя точками");
     }
 }
